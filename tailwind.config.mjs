5f085017b7fc51327f698202553a842d06d3b887
shenkzjay <senksjay@gmail.com>
2024-07-19 13:29:00 +0100

    added darkmode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: "selector",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "375px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "840px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1110px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "4850px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "5536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+};
