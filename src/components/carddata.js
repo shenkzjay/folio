36d0a290b01156f3b826fae3d96baafebbbc6eb5
shenkzjay <senksjay@gmail.com>
2025-02-17 15:56:14 +0100

    added projects

---
+++ src/components/carddata.js
@@ -0,0 +1,58 @@
+import AI from "../../public/vid/ai vid.webm";
+import estate from "../../public/vid/estatennn.webm";
+
+export const cards = [
+  {
+    title: "RAG AI agent chatbot",
+    link: "",
+    videolink: `${AI}`,
+    slug: "ai-agent-bot",
+  },
+  {
+    title: "Estate management system",
+    link: "",
+    videolink: `${estate}`,
+    slug: "estate-management",
+  },
+  {
+    title: "Online Resume Creator",
+    link: "",
+    videolink:
+      "https://ik.imagekit.io/tke5fzwem/newrsume.webm/ik-video.mp4?updatedAt=1739371539134",
+    slug: "online-resume-creator",
+  },
+  {
+    title: "Telegram clicker game app",
+    link: "",
+    videolink: "https://ik.imagekit.io/tke5fzwem/coin.webm/ik-video.mp4?updatedAt=1739371533953",
+    slug: "telegram-click-app",
+  },
+  {
+    title: "Football league ranking app",
+    link: "",
+    videolink: "https://ik.imagekit.io/tke5fzwem/proed.mp4?updatedAt=1739371582651",
+    slug: "football-ranking-app",
+  },
+
+  {
+    title: "React-Online-Code-Editor",
+    link: "",
+    videolink:
+      "https://ik.imagekit.io/tke5fzwem/neweditor.webm/ik-video.mp4?updatedAt=1739371534730",
+    slug: "react-online-code-editor",
+  },
+
+  {
+    title: "E-commerce Price Checker",
+    link: "",
+    videolink: "https://ik.imagekit.io/tke5fzwem/newecom.webm/ik-video.mp4?updatedAt=1739371542713",
+    slug: "ecommerce-price-comparison",
+  },
+
+  {
+    title: "Css Art Illustrations",
+    link: "",
+    videolink: "https://ik.imagekit.io/tke5fzwem/cssarts.webm/ik-video.mp4?updatedAt=1739371534205",
+    slug: "css-arts",
+  },
+];
